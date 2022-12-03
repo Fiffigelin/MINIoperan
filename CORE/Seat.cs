5c55290025ebab9885b18ca4689a8ec2df4483b3
Elin Nyman <elin.nyman87@gmail.com>
2022-12-03 11:40:51 +0100

    Seat.cs created

---
+++ CORE/Seat.cs
@@ -0,0 +1,15 @@
+class Seat
+{
+    public int Id {get; set;}
+    public string Section {get; set;}
+    public int Row {get; set;}
+    public int Price {get; set;}
+
+    public Seat (int id, string section, int row, int price)
+    {
+        Id = id;
+        Section = section;
+        Row = row;
+        Price = price;
+    }
+}
